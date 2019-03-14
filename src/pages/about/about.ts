a598431ed354ca190c215be38a029484c6227287
Luke W. McCullough <lukaswm@gmail.com>
2019-03-14 16:58:36 -0400

    inital commit

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+// import '@winjs';
+//import ExchangeActiveSyncProvisioning = Windows.Security.ExchangeActiveSyncProvisioning;
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
+declare var currentDeviceInfor: Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation;
+
 export class AboutPage {
 
   constructor(public navCtrl: NavController) {
-
+	  document.getElementById("DeviceInforLaunch").addEventListener("click", this.deviceInfor, false);
+    document.getElementById("DeviceInforReset").addEventListener("click", this.deviceInforReset, false);
   }
+
+  deviceInfor() {
+        document.getElementById("DeviceID").innerText = currentDeviceInfor.id;
+        document.getElementById("OperatingSystem").innerText = currentDeviceInfor.operatingSystem;
+        document.getElementById("FriendlyName").innerText = currentDeviceInfor.friendlyName;
+        document.getElementById("SystemManufacturer").innerText = currentDeviceInfor.systemManufacturer;
+        document.getElementById("SystemProductName").innerText = currentDeviceInfor.systemProductName;
+        document.getElementById("SystemSku").innerText = currentDeviceInfor.systemSku;
+    }
+
+   deviceInforReset() {
+        document.getElementById("DeviceID").innerText = "";
+        document.getElementById("OperatingSystem").innerText = "";
+        document.getElementById("FriendlyName").innerText = "";
+        document.getElementById("SystemManufacturer").innerText = "";
+        document.getElementById("SystemProductName").innerText = "";
+        document.getElementById("SystemSku").innerText = "";
+    }
 
 }
